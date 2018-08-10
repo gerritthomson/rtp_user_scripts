3b87fa97a71c2b62f80a2fd1fd73cb45b2199c12
gerrit thomson <gerrit.thomson@gmail.com>
2018-08-11 08:26:36 +1000

    set version in code.

--- Td Display Block.user.js
+++ Td Display Block.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Td Display Block
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    http://gerrit.com.au/
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://estore.skifalls.com.au/WebAdmin/instructorTools/*
