11e52fa256c60743703ef5abaa8eb765c5834b33
gerrit thomson <gerrit.thomson@gmail.com>
2018-08-11 17:05:56 +1000

    update version.

--- Td Display Block.user.js
+++ Td Display Block.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Td Display Block
 // @namespace    http://gerrit.com.au/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://estore.skifalls.com.au/WebAdmin/instructorTools/*
